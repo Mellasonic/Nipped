6f8b7f07e6c29c8da431534a6c056890eeed5b3a
Daniel Ye <danielyeqt@gmail.com>
2022-05-10 10:05:44 -0400

    Add Interactivity

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,9 +1,11 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
@@ -12,18 +14,30 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        // create sample content
-        Rectangle rect = new Rectangle(100, 100, 200, 300);
-        Pane root = new Pane(rect);
-        root.setPrefSize(500, 500);
+        Rectangle a = new Rectangle(100, 100, 200, 200);
+        Rectangle b = new Rectangle(200, 200, 300, 300);
+        Pane p1 = new Pane(a);
+        Pane p2 = new Pane(b);
+        p1.setPrefSize(500, 500);
+        p2.setPrefSize(500, 500);
+        Scene s1 = new Scene(p1);
+        Scene s2 = new Scene(p2);
 
-        // create scene containing the content
-        Scene scene = new Scene(root);
-        primaryStage.setScene(scene);
+        p1.setOnMousePressed(new EventHandler<MouseEvent>(){
+            @Override
+            public void handle(MouseEvent me){
+                if(100 <= me.getX() && me.getX() <= 300 && 100 <= me.getY() && me.getY() <= 300)
+                    primaryStage.setScene(s2);
+            }
+        });
 
+        p2.setOnMousePressed(me -> {
+            primaryStage.setScene(s1);
+        });
+
+        primaryStage.setScene(s1);
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
