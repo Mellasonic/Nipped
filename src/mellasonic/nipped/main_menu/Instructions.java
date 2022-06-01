f2e98276b50a0befc329651d6f679faf68e351b7
Daniel Ye <danielyeqt@gmail.com>
2022-06-01 09:04:21 -0400

    Finish Instructions page

--- src/mellasonic/nipped/main_menu/Instructions.java
+++ src/mellasonic/nipped/main_menu/Instructions.java
@@ -30,6 +30,7 @@ import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import mellasonic.nipped.AppState;
 import mellasonic.nipped.Main;
 import mellasonic.nipped.Tools;
@@ -45,22 +46,52 @@ import java.util.List;
 
 import static mellasonic.nipped.Tools.getImage;
 
+/**
+ * The state in which users get their instructions
+ */
 public class Instructions implements AppState {
-
+    /**
+     * the text that appears on each page
+     */
     private static final List<String> pages = Arrays.asList(
-            "In the first level, you eat food",
+            "In the first level, you eat food and you eat more food and blob blob blob and more sample text which is yummy",
             "In the second level, you blob away"
     );
 
+    /**
+     * the screen
+     */
+    private final Pane screen;
+    /**
+     * the margins of the panel itself
+     */
     public static final int PANEL_MARGIN = 30;
+    /**
+     * the margins of the context from the panel
+     */
     public static final int CONTENT_MARGIN = 30;
+    /**
+     * dimensions of the arrow
+     */
+    public static final int ARROW_WIDTH=60, ARROW_HEIGHT=48;
 
+    public Instructions(){
+        // declare main pane
+        screen = new Pane();
+        screen.setPrefSize(Main.WIDTH, Main.HEIGHT);
+    }
+
+    /**
+     * Creates a node representing the current panel of the instructions
+     * @param page the page number
+     * @return a node displaying the panel
+     */
     private Pane paneOf(int page){
         int width = Main.WIDTH - 2 * PANEL_MARGIN;
         int height = Main.HEIGHT - 2 * PANEL_MARGIN;
         Pane p = new Pane();
 
-        ImageView panel = new ImageView(Tools.getImage("assets/panel.png"));
+        ImageView panel = new ImageView(getImage("assets/panel.png"));
         panel.setFitWidth(width);
         panel.setFitHeight(height);
         p.getChildren().add(panel);
@@ -73,30 +104,67 @@ public class Instructions implements AppState {
         text.setFont(new Font("Bell MT", 24));
         p.getChildren().add(text);
 
+        if(page > 0){
+            ImageView leftArrow = new ImageView(getImage("assets/R-redarrow.png"));
+            leftArrow.setFitWidth(ARROW_WIDTH);
+            leftArrow.setFitHeight(ARROW_HEIGHT);
+            leftArrow.setLayoutX(CONTENT_MARGIN);
+            leftArrow.setLayoutY(height - CONTENT_MARGIN - ARROW_HEIGHT);
+
+            Text left = new Text("Previous");
+            left.setFont(new Font("Baskerville Old Face", 18));
+            left.setTextOrigin(VPos.CENTER);
+            left.setLayoutX(CONTENT_MARGIN + ARROW_WIDTH + 5);
+            left.setLayoutY(height - CONTENT_MARGIN - ARROW_HEIGHT / 2.0);
+
+            leftArrow.setOnMouseClicked(me -> {
+                Pane add = paneOf(page - 1);
+                add.setLayoutX(PANEL_MARGIN);
+                add.setLayoutY(PANEL_MARGIN);
+                screen.getChildren().set(1, add);
+            });
+
+            p.getChildren().add(leftArrow);
+            p.getChildren().add(left);
+        }
+        if(page < pages.size() - 1){
+            ImageView rightArrow = new ImageView(getImage("assets/R-greenarrow.png"));
+            rightArrow.setFitWidth(ARROW_WIDTH);
+            rightArrow.setFitHeight(ARROW_HEIGHT);
+            rightArrow.setLayoutX(width - CONTENT_MARGIN - ARROW_WIDTH);
+            rightArrow.setLayoutY(height - CONTENT_MARGIN - ARROW_HEIGHT);
+
+            Text right = new Text("Next");
+            right.setFont(new Font("Baskerville Old Face", 18));
+            right.setTextOrigin(VPos.CENTER);
+            right.setTextAlignment(TextAlignment.RIGHT);
+            right.setWrappingWidth(100);
+            right.setLayoutX(width - CONTENT_MARGIN - ARROW_WIDTH - 100);
+            right.setLayoutY(height - CONTENT_MARGIN - ARROW_HEIGHT / 2.0);
+
+            rightArrow.setOnMouseClicked(me -> {
+                Pane add = paneOf(page + 1);
+                screen.getChildren().set(1, add);
+            });
+
+            p.getChildren().add(rightArrow);
+            p.getChildren().add(right);
+        }
+        p.setLayoutX(PANEL_MARGIN);
+        p.setLayoutY(PANEL_MARGIN);
+
         return p;
     }
 
     @Override
     public Scene getScene(){
-        // declare main pane
-        Pane p = new Pane();
-        p.setPrefSize(Main.WIDTH, Main.HEIGHT);
-
         // initialize background image & add to pane
         ImageView background = new ImageView(getImage("assets/livingroom.png"));
         background.setFitWidth(Main.WIDTH);
         background.setFitHeight(Main.HEIGHT);
-        p.getChildren().add(background);
+        screen.getChildren().add(background);
 
-        ArrayList<Pane> nodes = new ArrayList<>();
-        for(int i = 0; i < pages.size(); i++){
-            Pane add = paneOf(i);
-            add.setLayoutX(PANEL_MARGIN);
-            add.setLayoutY(PANEL_MARGIN);
-            nodes.add(add);
-        }
-
-        p.getChildren().add(nodes.get(0));
-        return new Scene(p);
+        screen.getChildren().add(paneOf(0));
+        return new Scene(screen);
     }
 }
