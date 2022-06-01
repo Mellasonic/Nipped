6cbe82fd395870f4e7644f799850620728c3bdd2
Daniel Ye <danielyeqt@gmail.com>
2022-06-01 10:03:33 -0400

    Add base Level 3 Classes

---
+++ src/mellasonic/nipped/game/point_and_click/locations/level3/Bedroom.java
@@ -0,0 +1,17 @@
+package mellasonic.nipped.game.point_and_click.locations.level3;
+
+// imports
+
+
+/**
+ * The first room - a living room in level 1 and 3
+ */
+public abstract class Bedroom extends mellasonic.nipped.game.point_and_click.locations.Bedroom {
+
+    /**
+     * Class constructor
+     */
+    public Bedroom() {
+
+    }
+}
