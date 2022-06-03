9974464dcedc404cab99964990eb798f8de4021b
James Huynh <james.huynh444@gmail.com>
2022-06-03 09:26:54 -0400

    atticgs and a bit more

--- src/mellasonic/nipped/main_menu/leaderboard/Score.java
+++ src/mellasonic/nipped/main_menu/leaderboard/Score.java
@@ -1,8 +1,41 @@
+/* ICS Final Project Nipped
+ 2022/06/03
+ Time spent: 30 mins
+*/
+
+/*
+ Nipped is a java game with three different levels.
+ Version 1.0 - June 3, 2022
+ Authors: Daniel Ye, James Huynh, Eric Jin
+*/
+
+/*
+ Modification Authors: James Huynh, Daniel Ye
+ Version 1.0
+ 2022/06/03
+ Time spent: 30 mins
+ New features/processing: Added ability to store scores as an object in this class
+*/
+
 package mellasonic.nipped.main_menu.leaderboard;
 
+/**
+* This class stores a singular score, by storing the name and its corresponding score
+*/
 public class Score {
+   /**
+   * This variable stores the score of the user
+   */
     public final int score;
+    /**
+    * This variable stores the name of the user
+    */
     public final String name;
+    /**
+    * Class constructor
+    * @param name The name of the user
+    * @param score The score of the user
+    */
     public Score(String name, int score){
         this.name = name;
         this.score = score;
--- src/mellasonic/nipped/main_menu/leaderboard/ScoreReader.java
+++ src/mellasonic/nipped/main_menu/leaderboard/ScoreReader.java
@@ -1,9 +1,34 @@
+/* ICS Final Project Nipped
+ 2022/06/03
+ Time spent: 40 mins
+*/
+
+/*
+ Nipped is a java game with three different levels.
+ Version 1.1 - June 3, 2022
+ Authors: Daniel Ye, James Huynh, Eric Jin
+*/
+
+/*
+ Modification Authors: Daniel Ye
+ Version 1.0
+ 2022/06/03
+ Time spent: 10 mins
+ New features/processing: Added template
+*/
+/*
+ Modification Authors: James Huynh
+ Version 1.1
+ 2022/06/03
+ Time spent: 30 mins
+ New features/processing: Added functionality
+*/
 package mellasonic.nipped.main_menu.leaderboard;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class ScoreReader {
     public static ArrayList<Score> getScores(){
-        return null;
+        
     }
 }
