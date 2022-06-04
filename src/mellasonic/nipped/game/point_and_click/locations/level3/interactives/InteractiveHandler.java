6587674ba73f5abcbac26bfdf1d45333265b822f
Daniel Ye <danielyeqt@gmail.com>
2022-06-03 20:29:08 -0400

    Add Some interactives (catclean, food, drawer)

--- src/mellasonic/nipped/game/point_and_click/locations/level3/interactives/InteractiveHandler.java
+++ src/mellasonic/nipped/game/point_and_click/locations/level3/interactives/InteractiveHandler.java
@@ -1,3 +1,22 @@
+/* ICS Final Project Nipped
+ 2022/06/03
+ Time spent: 10 min
+*/
+
+/*
+ Nipped is a java game with three different levels.
+ Version 1.1 - 3 June 2022
+ Authors: Daniel Ye, James Huynh, Eric Jin
+*/
+
+/*
+ Modification Authors: Eric Jin
+ Version 1.1
+ 2022/06/03
+ Time spent: 10 min
+ New features/processing: create interactive handler
+*/
+
 package mellasonic.nipped.game.point_and_click.locations.level3.interactives;
 
 public interface InteractiveHandler {
