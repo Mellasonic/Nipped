762c78a8ace47aadfd6db0155b7f0e20af4af13c
James Huynh <james.huynh444@gmail.com>
2022-06-06 09:44:16 -0400

    unfinished changes

--- src/mellasonic/nipped/game/point_and_click/Level1.java
+++ src/mellasonic/nipped/game/point_and_click/Level1.java
@@ -61,81 +61,83 @@ import mellasonic.nipped.game.point_and_click.locations.level1.*;
  * the first level
  */
 public abstract class Level1 extends PointClick{
-    /**
-     * the number of consumables consumed
-     */
-    private int numConsumed = 0;
-    /**
-     * the total number of consumables
-     */
-    private static final int totObject = 11;
-    /**
-     * the door
-     */
-    private final Door door;
+   /**
+    * the number of consumables consumed
+    */
+   private int numConsumed = 0;
+   /**
+    * the total number of consumables
+    */
+   private static final int totObject = 11;
+   /**
+    * the door
+    */
+   private final Door door;
 
-    /**
-     * Class constructor
-     */
-    public Level1() {
-        super();
-
-        // initialize handlers
-        ConsumableHandler handler = consumed -> {
+   /**
+    * Class constructor
+    */
+   public Level1() {
+      super();
+   
+      // initialize handlers
+      ConsumableHandler handler = 
+         consumed -> {
             consumed.disable();
             onConsume();
-        };
-
-        ScreenChanger changer = this::changeScreen;
-
-        // initialize rooms
-        LivingRoom living = new LivingRoom(handler, changer);
-        Kitchen kitchen = new Kitchen(handler, changer);
-        Bedroom bedroom = new Bedroom(handler, changer);
-        BedroomDrawer bDrawer = new BedroomDrawer(handler, changer);
-        KitchenDrawer kDrawer = new KitchenDrawer(handler, changer);
-        Attic attic = new Attic(handler, changer);
-        door = new Door(changer, true) {
+         };
+   
+      ScreenChanger changer = this::changeScreen;
+   
+      // initialize rooms
+      LivingRoom living = new LivingRoom(handler, changer);
+      Kitchen kitchen = new Kitchen(handler, changer);
+      Bedroom bedroom = new Bedroom(handler, changer);
+      BedroomDrawer bDrawer = new BedroomDrawer(handler, changer);
+      KitchenDrawer kDrawer = new KitchenDrawer(handler, changer);
+      Attic attic = new Attic(handler, changer);
+      door = 
+         new Door(changer, true) {
             @Override
             public void onDoorClicked() {
-                nextLevel();
+               nextLevel();
             }
-        };
+         };
+   
+      // link rooms to each other
+      living.setKitchen(kitchen);
+      living.setBedroom(bedroom);
+      living.setDoor(door);
+      door.setLiving(living);
+      kitchen.setLiving(living);
+      kitchen.setDrawer(kDrawer);
+      kDrawer.setPrev(kitchen);
+      bedroom.setLiving(living);
+      bedroom.setAttic(attic);
+      bedroom.setDrawer(bDrawer);
+      bDrawer.setPrev(bedroom);
+      attic.setBedroom(bedroom);
+   
+      // go to a living room screen
+      changeScreen(living);
+      System.out.println(Font.getFamilies());
+   }
 
-        // link rooms to each other
-        living.setKitchen(kitchen);
-        living.setBedroom(bedroom);
-        living.setDoor(door);
-        door.setLiving(living);
-        kitchen.setLiving(living);
-        kitchen.setDrawer(kDrawer);
-        kDrawer.setPrev(kitchen);
-        bedroom.setLiving(living);
-        bedroom.setAttic(attic);
-        bedroom.setDrawer(bDrawer);
-        bDrawer.setPrev(bedroom);
-        attic.setBedroom(bedroom);
-
-        // go to a living room screen
-        changeScreen(living);
-        System.out.println(Font.getFamilies());
-    }
-
-    /**
-     * Called when something is consumed
-     */
-    private void onConsume(){
-        ++numConsumed;
-        if(numConsumed == totObject) door.enable();
-        if(numConsumed == 3){
-            Screen prev = getCurScreen();
-            Panel display = new Panel("Hello this is a super duper cool text", prev.getNode()){
-                @Override
-                public void onExit() {
-                    changeScreen(prev);
-                }
-            };
-            changeScreen(display);
-        }
-    }
+   /**
+    * Called when something is consumed
+    */
+   private void onConsume(){
+      ++numConsumed;
+      if(numConsumed == totObject) door.enable();
+      Screen prev = getCurScreen();
+      anel display = 
+         new Panel("Hello this is a super duper cool text", prev.getNode()){
+            @Override
+            public void onExit() {
+               changeScreen(prev);
+            }
+         };
+      changeScreen(display);
+      
+   }
 }
